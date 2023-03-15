1d055ab2b946d7c6e30789ff1a56dec193f3be6b
Rida Salman <ridasalman458@gmail.com>
2023-03-15 22:00:44 +0500

    Flutter Youtube API & Video Player

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_6/screens/home_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter YouTube API',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.redAccent,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
---
+++ lib/utilities/keys.dart
@@ -0,0 +1,1 @@
+const String API_KEY = 'AIzaSyDQMzLiKmqERMjQYpdoODSXt7RS8zBJ1gI';
